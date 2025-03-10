9f7e2aa77a37403f33ff5c382c14bf06ed51e2c3
DmitryG228 <2280905@gmail.com>
2025-03-10 15:29:37 +0300

    setup database on start

--- setup_database.py
+++ setup_database.py
@@ -1,21 +1,22 @@
-
 import asyncio
 from sqlalchemy.ext.asyncio import create_async_engine
-from psql_models import Base, DATABASE_URL
+from models.db import Base, DATABASE_URL
 
-from dashboard.services.psql_helpers import _compile_drop_table
+from services.psql_helpers import _compile_drop_table
 
 
-async def init_db():
+async def init_db(drop_tables=False):
     engine = create_async_engine(DATABASE_URL)
     
     async with engine.begin() as conn:
-        # Drop all tables with CASCADE
-        async def drop_all_tables():
-            async with engine.begin() as conn:
-                await conn.run_sync(Base.metadata.drop_all)
-                
-        await drop_all_tables()
+        # Drop all tables with CASCADE if flag is True
+        if drop_tables:
+            async def drop_all_tables():
+                async with engine.begin() as conn:
+                    await conn.run_sync(Base.metadata.drop_all)
+                    
+            await drop_all_tables()
+            print("All tables dropped.")
             
         # Create all tables
         await conn.run_sync(Base.metadata.create_all)
@@ -23,4 +24,4 @@ async def init_db():
     print("Database initialized successfully.")
 
 if __name__ == "__main__":
-    asyncio.run(init_db()) 
+    asyncio.run(init_db(drop_tables=False)) 
